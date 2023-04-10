fbcde1c2d99eb1511ef8fae5676f0c2fc9b579b7
Im0ne <116801019+Im0ne@users.noreply.github.com>
2023-04-10 17:04:05 +0200

    Math lib (#5)
    * Update math_tests.py
    
    * Create math_lib.py
    
    * Update math_tests.py

--- src/math_tests.py
+++ src/math_tests.py
@@ -1,4 +1,4 @@
-import calc
+from src/math_lib import MathLib as calc
 import unittest
 
 class TestMathLibrary(unittest.TestCase):
@@ -9,36 +9,36 @@ class TestMathLibrary(unittest.TestCase):
         self.assertEqual(calc.add(3, 2), 5)
 
     def test_subtract(self):
-        self.assertEqual(calc.subtract(1, 2), -1)
-        self.assertEqual(calc.subtract(2, 2), 0)
-        self.assertEqual(calc.subtract(3, 2), 1)
+        self.assertEqual(calc.sub(1, 2), -1)
+        self.assertEqual(calc.sub(2, 2), 0)
+        self.assertEqual(calc.sub(3, 2), 1)
     
     def test_multiply(self):
-        self.assertEqual(calc.multiply(1, 2), 2)
-        self.assertEqual(calc.multiply(2, 2), 4)
-        self.assertEqual(calc.multiply(3, 2), 6)
+        self.assertEqual(calc.mul(1, 2), 2)
+        self.assertEqual(calc.mul(2, 2), 4)
+        self.assertEqual(calc.mul(3, 2), 6)
 
     def test_divide(self):
-        self.assertEqual(calc.divide(1, 2), 0.5)
-        self.assertEqual(calc.divide(2, 2), 1)
-        self.assertEqual(calc.divide(3, 2), 1.5)
+        self.assertEqual(calc.div(1, 2), 0.5)
+        self.assertEqual(calc.div(2, 2), 1)
+        self.assertEqual(calc.div(3, 2), 1.5)
 
     def test_power(self):
-        self.assertEqual(calc.power(1, 2), 1)
-        self.assertEqual(calc.power(2, 2), 4)
-        self.assertEqual(calc.power(3, 2), 9)
+        self.assertEqual(calc.pow(1, 2), 1)
+        self.assertEqual(calc.pow(2, 2), 4)
+        self.assertEqual(calc.pow(3, 2), 9)
     
     def test_root(self):
         self.assertEqual(calc.root(1, 2), 1)
         self.assertEqual(calc.root(4, 2), 2)
-        self.assertEqual(calc.root(9, 2), 3)
+        self.assertEqual(calc.root(8, 3), 2)
 
     def test_factorial(self):
-        self.assertEqual(calc.factorial(1), 1)
-        self.assertEqual(calc.factorial(2), 2)
-        self.assertEqual(calc.factorial(3), 6)
+        self.assertEqual(calc.fact(1), 1)
+        self.assertEqual(calc.fact(2), 2)
+        self.assertEqual(calc.fact(3), 6)
 
-    def test_logarithm(self):
-        self.assertEqual(calc.logarithm(1, 2), 0)
-        self.assertEqual(calc.logarithm(2, 2), 1)
-        self.assertEqual(calc.logarithm(3, 2), 1.5849625007211563)
+    def test_absolute(self):
+        self.assertEqual(calc.abs(1), 1)
+        self.assertEqual(calc.abs(-1), 1)
+        self.assertEqual(calc.abs(-235233), 235233)
